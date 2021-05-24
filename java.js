43ff5440e62ae1c75f47943add305c14a0ea3280
jarjarjarjarjarjar <75090491+jarjarjarjarjarjar@users.noreply.github.com>
2021-05-24 15:49:03 -0500

    Add files via upload

--- java.js
+++ java.js
@@ -1,127 +1,38 @@
-var canvas = new fabric.Canvas('Mycanvas');
-var height = 1000;
-var width = 1000;
-var x = 100;
-var y = 100;
-var player = "";
-var block = "";
-function player_update() {
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+Width=0;
+Height=0;
+player_x=30
+player_y=30
+Player=""
+Block_image=""
+
+function player_update(){
     fabric.Image.fromURL("player.png",
-        function (Img) {
-            player = Img;
-            player.scaleToWidth(150);
-            player.scaleToHeight(140);
-            player.set({
-                top: y,
-                left: x
-            });
-            canvas.add(player);
+    function (Img) {
+        Player = Img;
+        Player.scaleToWidth(150);
+        Player.scaleToHeight(140);
+        Player.set({
+            top: player_y,
+            left: player_x
         });
+        canvas.add(player_object);
+    });
 }
 
 function new_image(get_image) {
     fabric.Image.fromURL(get_image, function (Img) {
-        block = Img;
+        Block_image = Img;
 
-        block.scaleToWidth(width);
-        block.scaleToHeight(height);
-        block.set({
-            top: y,
-            left: x
+        Block_image.scaleToWidth(width);
+        Block_image.scaleToHeight(height);
+        Block_image.set({
+            top: player_y,
+            left: player_x
         });
-        canvas.add(block);
+        canvas.add(Block_image);
     });
-}
-window.addEventListener("keydown", my_keydown);
-function my_keydown(e) {
-    keyPressed = e.keyCode;
-    if (e.shiftkey == true && keyPressed == '73') {
-        height = height + 10
-        width = width + 10
-    }
-    if (e.shiftkey == true && keyPressed == '68') {
-        height = height - 10
-        width = width - 10
-    }
-    if (keyPressed == '66' && keyPressed == '83') {
-        new_image('spiderman_body.png')
-        console.log('b and s')
-    }
-    if (keyPressed == '66' && keyPressed == '72') {
-        new_image('hulk_body.png')
-    }
-    if (keyPressed == '66' && keyPressed == '73') {
-        new_image('ironman_body.png')
-    }
-    if (keyPressed == '76' && keyPressed == '83') {
-        new_image("spiderman_legs.png")
-    }
-    if (keyPressed == '76' && keyPressed == '72') {
-        new_image("hulk_legs.png")
-    }
-    if (keyPressed == '76' && keyPressed == '73') {
-        new_image("ironman_legs.png")
-    }
-    if (keyPressed == '70' && keyPressed == '83') {
-        new_image("spiderman_face.png")
-    }
-    if (keyPressed == '70' && keyPressed == '72') {
-        new_image("hulk_face.png")
-    }
-    if (keyPressed == '70' && keyPressed == '73') {
-        new_image("ironman_face.png")
-    }
-    if (keyPressed == '70' && keyPressed == '84') {
-        new_image("thor_face.png")
-    }
-    if (keyPressed == '90' && keyPressed == '83') {
-        new_image("spiderman_left_hand.png")
-    }
-    if (keyPressed == '90' && keyPressed == '72') {
-        new_image("hulk_left_hand.png")
-    }
-    if (keyPressed == '90' && keyPressed == '73') {
-        new_image("ironman_left_hand.png")
-    }
-    if (keyPressed == '90' && keyPressed == '84') {
-        new_image("thor_left_hand.png")
-    }
-    if (keyPressed == '89' && keyPressed == '83') {
-        new_image("spiderman_right_hand.png")
-    }
-    if (keyPressed == '89' && keyPressed == '72') {
-        new_image("hulk_right_hand.png")
-    }
-    if (keyPressed == '89' && keyPressed == '73') {
-        new_image("ironman_right_hand.png")
-    }
-    if (keyPressed == '89' && keyPressed == '84') {
-        new_image("thor_right_hand.png")
-    }
-}
-function up() {
-    if (y >= 0) {
-        y = y - block
-        canvas.remove(player);
-        player_update();
-    }
-}
-function down() {
-    if (y <= 500) {
-        y = y + height;
-        canvas.remove(player); player_update();
-    }
-}
-function left() 
-{ 
-    if (x > 0) { x = x - width;
-        canvas.remove(player); player_update(); 
-    } 
-}
-function right() 
-{
-     if (x <= 850) { x = x + width;
-        canvas.remove(player); player_update(); 
-    } 
 }
 
